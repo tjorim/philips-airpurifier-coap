3f221d315e0934857109a7108af2ed283ae49c08
Jakub Rusiłko <brodaty.bosman@gmail.com>
2021-04-27 09:12:07 +0200

    feat: support for ac2958 (#43)

--- custom_components/philips_airpurifier_coap/fan.py
+++ custom_components/philips_airpurifier_coap/fan.py
@@ -81,6 +81,7 @@ from .const import (
     MODEL_AC2729,
     MODEL_AC2889,
     MODEL_AC2939,
+    MODEL_AC2958,
     MODEL_AC3059,
     MODEL_AC3829,
     MODEL_AC3858,
@@ -152,6 +153,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
                 MODEL_AC2729,
                 MODEL_AC2889,
                 MODEL_AC2939,
+                MODEL_AC2958,
                 MODEL_AC3059,
                 MODEL_AC3829,
                 MODEL_AC3858,
@@ -180,6 +182,7 @@ async def async_setup_platform(
         MODEL_AC2729: PhilipsAC2729,
         MODEL_AC2889: PhilipsAC2889,
         MODEL_AC2939: PhilipsAC2939,
+        MODEL_AC2958: PhilipsAC2958,
         MODEL_AC3059: PhilipsAC3059,
         MODEL_AC3829: PhilipsAC3829,
         MODEL_AC3858: PhilipsAC3858,
@@ -620,6 +623,14 @@ class PhilipsAC2889(PhilipsGenericCoAPFan):
     }
 
 class PhilipsAC2939(PhilipsTVOCMixin, PhilipsGenericCoAPFan):
+    AVAILABLE_PRESET_MODES = {
+        PRESET_MODE_AUTO: {PHILIPS_POWER: "1", PHILIPS_MODE: "AG"},
+        PRESET_MODE_GENTLE: {PHILIPS_POWER: "1", PHILIPS_MODE: "GT"},
+        PRESET_MODE_SLEEP: {PHILIPS_POWER: "1", PHILIPS_MODE: "S"},
+        PRESET_MODE_TURBO: {PHILIPS_POWER: "1", PHILIPS_MODE: "T"},
+    }
+
+class PhilipsAC2958(PhilipsGenericCoAPFan):
     AVAILABLE_PRESET_MODES = {
         PRESET_MODE_AUTO: {PHILIPS_POWER: "1", PHILIPS_MODE: "AG"},
         PRESET_MODE_GENTLE: {PHILIPS_POWER: "1", PHILIPS_MODE: "GT"},
