4be093716e0de7b4d84834e892c8eff4fef26bbd
Pavel Skripkin <skripkin.pavel@gmail.com>
2021-05-02 13:04:30 +0100

    Add support for Philips AirPurifier AC3033 (#45)

--- custom_components/philips_airpurifier_coap/fan.py
+++ custom_components/philips_airpurifier_coap/fan.py
@@ -82,6 +82,7 @@ from .const import (
     MODEL_AC2889,
     MODEL_AC2939,
     MODEL_AC2958,
+    MODEL_AC3033,
     MODEL_AC3059,
     MODEL_AC3829,
     MODEL_AC3858,
@@ -154,6 +155,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
                 MODEL_AC2889,
                 MODEL_AC2939,
                 MODEL_AC2958,
+                MODEL_AC3033,
                 MODEL_AC3059,
                 MODEL_AC3829,
                 MODEL_AC3858,
@@ -183,6 +185,7 @@ async def async_setup_platform(
         MODEL_AC2889: PhilipsAC2889,
         MODEL_AC2939: PhilipsAC2939,
         MODEL_AC2958: PhilipsAC2958,
+        MODEL_AC3033: PhilipsAC3033,
         MODEL_AC3059: PhilipsAC3059,
         MODEL_AC3829: PhilipsAC3829,
         MODEL_AC3858: PhilipsAC3858,
@@ -638,6 +641,14 @@ class PhilipsAC2958(PhilipsGenericCoAPFan):
         PRESET_MODE_TURBO: {PHILIPS_POWER: "1", PHILIPS_MODE: "T"},
     }
 
+class PhilipsAC3033(PhilipsTVOCMixin, PhilipsGenericCoAPFan):
+    AVAILABLE_PRESET_MODES = {
+        PRESET_MODE_SPEED_1: {PHILIPS_POWER: "1", PHILIPS_MODE: "M", PHILIPS_SPEED: "1"},
+        PRESET_MODE_SPEED_2: {PHILIPS_POWER: "1", PHILIPS_MODE: "M", PHILIPS_SPEED: "2"},
+        PRESET_MODE_AUTO: {PHILIPS_POWER: "1", PHILIPS_MODE: "AG"},
+        PRESET_MODE_SLEEP: {PHILIPS_POWER: "1", PHILIPS_MODE: "S", PHILIPS_SPEED: "s"},
+        PRESET_MODE_TURBO: {PHILIPS_POWER: "1", PHILIPS_MODE: "T", PHILIPS_SPEED: "t"},
+    }
 
 class PhilipsAC3059(PhilipsTVOCMixin, PhilipsGenericCoAPFan):
     AVAILABLE_PRESET_MODES = {
