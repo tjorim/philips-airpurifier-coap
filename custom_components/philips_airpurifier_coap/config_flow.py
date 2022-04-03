45c6ae8faab54fb8f7d54b48e980a13f9d363ff3
kongo09 <kongo09@users.noreply.github.com>
2022-04-03 16:14:39 +0200

    start work on config_flow

--- custom_components/philips_airpurifier_coap/config_flow.py
+++ custom_components/philips_airpurifier_coap/config_flow.py
@@ -1,42 +1,53 @@
-"""The HHS Vertretungsplan component."""
-from homeassistant import config_entries
+"""The Philips AirPurifier component."""
+from homeassistant import config_entries, exceptions
 from homeassistant.data_entry_flow import FlowResult
 
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
-
-from hhs_vertretungsplan_parser.vertretungsplan_parser import HHSVertretungsplanParser, AuthenticationException
+from homeassistant.const import CONF_HOST
 
 from aiohttp.client_exceptions import ClientConnectorError
+from aioairctrl import CoAPClient
+
+from .const import DOMAIN
+from .philips import Coordinator
 
 from typing import Any
 
 import logging
 import voluptuous as vol
+import ipaddress
 import re
-
-from .const import CONF_PASS, CONF_TUTOR_GROUP, CONF_USER, DEFAULT_NAME, DOMAIN, PREFIX
 
 _LOGGER = logging.getLogger(__name__)
 
 
+def host_valid(host: str) -> bool:
+    """Return True if hostname or IP address is valid."""
+    try:
+        if ipaddress.ip_address(host).version in [4, 6]:
+            return True
+    except ValueError:
+        pass
+    disallowed = re.compile(r"[^a-zA-Z\d\-]")
+    return all(x and not disallowed.search(x) for x in host.split("."))
 
-class HHSVertretungsplanConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+
+class PhilipsAirPurifierConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle config flow for HHS Vertretungsplan."""
 
     VERSION = 1
 
     def __init__(self) -> None:
         """Initialize."""
-        self.hhs: HHSVertretungsplanParser = None
+        self.coordinator: Coordinator = None
+        self.host: str = None
 
 
     def _get_schema(self, user_input):
         """Provide schema for user input."""
         schema = vol.Schema({
-            vol.Required(CONF_TUTOR_GROUP, default=user_input.get(CONF_TUTOR_GROUP, '')): cv.string,
-            vol.Required(CONF_USER, default=user_input.get(CONF_USER, '')): cv.string,
-            vol.Required(CONF_PASS, default=user_input.get(CONF_PASS, '')): cv.string
+            vol.Required(CONF_HOST, default=user_input.get(CONF_HOST, '')): cv.string
         })
         return schema
 
@@ -49,26 +60,29 @@ class HHSVertretungsplanConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         # user input was provided, so check and save it
         if user_input is not None:
             try:
-                # let's check the tutor group first and normalize
-                tutor_group = user_input[CONF_TUTOR_GROUP]
-                if tutor_group == None:
-                    raise TutorGroupError
-                tutor_group = re.sub('\s', '', tutor_group)
-                if len(tutor_group) == 0:
-                    raise TutorGroupError
-                user_input[CONF_TUTOR_GROUP] = tutor_group.lower()
+                # first some sanitycheck on the host input
+                if not host_valid(user_input[CONF_HOST]):
+                    raise InvalidHost()
+                self.host = user_input[CONF_HOST]
 
-                # let's try and connect to HHS
-                session = async_get_clientsession(self.hass)
-                user = user_input[CONF_USER]
-                password = user_input[CONF_PASS]
-                self.hhs = HHSVertretungsplanParser(session, user, password)
+                # let's try and connect to an AirPurifier
+                try:
+                    client = await CoAPClient.create(self.host)
+                except Exception as ex:
+                    _LOGGER.warning(r"Failed to connect: %s", ex)
+                    raise exceptions.ConfigEntryNotReady from ex
 
-                # try to load some data
-                await self.hhs.load_data()
+                self.coordinator = Coordinator(client)
+                await self.coordinator.async_first_refresh()
 
-                # use the tutor_group as unique_id
-                unique_id = user_input[CONF_TUTOR_GROUP]
+                # autodetect model and name
+                model = self.coordinator.status['type']
+                name = self.coordinator.status['name']
+                device_id = self.coordinator.status['DeviceId']
+                _LOGGER.debug("Detected host %s as model %s with name: %s", self.host, model, name)
+
+                # use the device ID as unique_id
+                unique_id = device_id
                 _LOGGER.debug(f"async_step_user: unique_id={unique_id}")
 
                 # set the unique id for the entry, abort if it already exists
@@ -77,14 +91,14 @@ class HHSVertretungsplanConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
                 # compile a name and return the config entry
                 return self.async_create_entry(
-                    title=PREFIX + " " + user_input[CONF_TUTOR_GROUP],
+                    title=model + " " + name,
                     data=user_input
                 )
 
-            except (ConnectionError, ClientConnectorError, AuthenticationException) as e:
-                errors['base'] = "authentication"
-            except TutorGroupError:
-                errors[CONF_TUTOR_GROUP] = "tutor_group"
+            except InvalidHost:
+                errors[CONF_HOST] = "host"
+            except exceptions.ConfigEntryNotReady:
+                errors[CONF_HOST] = "connect"
 
         if user_input is None:
             user_input = {}
@@ -96,5 +110,6 @@ class HHSVertretungsplanConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         # show the form to the user
         return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
 
-class TutorGroupError(Exception):
-    pass
+
+class InvalidHost(exceptions.HomeAssistantError):
+    """Error to indicate that hostname/IP address is invalid."""
