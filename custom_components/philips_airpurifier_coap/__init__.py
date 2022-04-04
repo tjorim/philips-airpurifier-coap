3abcf74e36ce1486242bff6ca854e0ed5979d029
kongo09 <kongo09@users.noreply.github.com>
2022-04-04 11:29:19 +0200

    complete fan setup

--- custom_components/philips_airpurifier_coap/__init__.py
+++ custom_components/philips_airpurifier_coap/__init__.py
@@ -30,23 +30,23 @@ from .const import (
 
 _LOGGER = logging.getLogger(__name__)
 
-CONFIG_SCHEMA = vol.Schema(
-    {
-        DOMAIN: vol.All(
-            cv.ensure_list,
-            [
-                vol.Schema(
-                    {
-                        vol.Required(CONF_HOST): cv.string,
-                        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-                        vol.Optional(CONF_ICON, default=DEFAULT_ICON): cv.icon,
-                    },
-                )
-            ],
-        )
-    },
-    extra=vol.ALLOW_EXTRA,
-)
+# CONFIG_SCHEMA = vol.Schema(
+#     {
+#         DOMAIN: vol.All(
+#             cv.ensure_list,
+#             [
+#                 vol.Schema(
+#                     {
+#                         vol.Required(CONF_HOST): cv.string,
+#                         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+#                         vol.Optional(CONF_ICON, default=DEFAULT_ICON): cv.icon,
+#                     },
+#                 )
+#             ],
+#         )
+#     },
+#     extra=vol.ALLOW_EXTRA,
+# )
 
 # PLATFORMS = ["fan", "sensor"]
 PLATFORMS = ["fan"]
@@ -101,44 +101,44 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     return True
 
 
-async def async_setup_old(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the Philips AirPurifier integration."""
-    hass.data[DOMAIN] = {}
+# async def async_setup_old(hass: HomeAssistant, config: ConfigType) -> bool:
+#     """Set up the Philips AirPurifier integration."""
+#     hass.data[DOMAIN] = {}
 
-    async def async_setup_air_purifier(conf: ConfigType):
-        host = conf[CONF_HOST]
+#     async def async_setup_air_purifier(conf: ConfigType):
+#         host = conf[CONF_HOST]
 
-        _LOGGER.debug("Setting up %s integration with %s", DOMAIN, host)
+#         _LOGGER.debug("Setting up %s integration with %s", DOMAIN, host)
 
-        try:
-            client = await CoAPClient.create(host)
-        except Exception as ex:
-            _LOGGER.warning(r"Failed to connect: %s", ex)
-            raise ConfigEntryNotReady from ex
+#         try:
+#             client = await CoAPClient.create(host)
+#         except Exception as ex:
+#             _LOGGER.warning(r"Failed to connect: %s", ex)
+#             raise ConfigEntryNotReady from ex
 
-        coordinator = Coordinator(client)
+#         coordinator = Coordinator(client)
 
-        hass.data[DOMAIN][host] = {
-            DATA_KEY_CLIENT: client,
-            DATA_KEY_COORDINATOR: coordinator,
-        }
+#         hass.data[DOMAIN][host] = {
+#             DATA_KEY_CLIENT: client,
+#             DATA_KEY_COORDINATOR: coordinator,
+#         }
 
-        await coordinator.async_first_refresh()
+#         await coordinator.async_first_refresh()
 
-        # autodetect model and name
-        model = coordinator.status['type']
-        name = coordinator.status['name']
-        conf[CONF_MODEL] = model
-        conf[CONF_NAME] = name
-        _LOGGER.debug("Detected host %s as model %s with name: %s", host, model, name)
+#         # autodetect model and name
+#         model = coordinator.status['type']
+#         name = coordinator.status['name']
+#         conf[CONF_MODEL] = model
+#         conf[CONF_NAME] = name
+#         _LOGGER.debug("Detected host %s as model %s with name: %s", host, model, name)
 
-        for platform in PLATFORMS:
-            hass.async_create_task(
-                discovery.async_load_platform(hass, platform, DOMAIN, conf, config)
-            )
+#         for platform in PLATFORMS:
+#             hass.async_create_task(
+#                 discovery.async_load_platform(hass, platform, DOMAIN, conf, config)
+#             )
 
-    tasks = [async_setup_air_purifier(conf) for conf in config[DOMAIN]]
-    if tasks:
-        await asyncio.wait(tasks)
+#     tasks = [async_setup_air_purifier(conf) for conf in config[DOMAIN]]
+#     if tasks:
+#         await asyncio.wait(tasks)
 
-    return True
+#     return True
--- custom_components/philips_airpurifier_coap/fan.py
+++ custom_components/philips_airpurifier_coap/fan.py
@@ -19,6 +19,7 @@ from .const import (
     DATA_KEY_CLIENT,
     DATA_KEY_COORDINATOR,
     DATA_KEY_FAN,
+    DEFAULT_ICON,
     DOMAIN,
 )
 
@@ -31,10 +32,58 @@ async def async_setup_entry(
     async_add_entities: Callable
 ):
     _LOGGER.debug("async_setup_entry called for platform fan")
-    return
+
+    host = entry.data[CONF_HOST]
+    model = entry.data[CONF_MODEL]
+    name = entry.data[CONF_NAME]
+
+    data = hass.data[DOMAIN][host]
+
+    model_class = model_to_class.get(model)
+    if model_class:
+        device = model_class(
+            data[DATA_KEY_CLIENT],
+            data[DATA_KEY_COORDINATOR],
+            model=model,
+            name=name,
+            icon=DEFAULT_ICON
+        )
+    else:
+        _LOGGER.error("Unsupported model: %s", model)
+        return
+
+    data[DATA_KEY_FAN] = device
+    async_add_entities([device], update_before_add=True)
+
+    def wrapped_async_register(
+        domain: str,
+        service: str,
+        service_func: Callable,
+        schema: Optional[vol.Schema] = None,
+    ):
+        async def service_func_wrapper(service_call):
+            service_data = service_call.data.copy()
+            entity_id = service_data.pop("entity_id", None)
+            devices = [
+                d
+                for entry in hass.data[DOMAIN].values()
+                if (d := entry[DATA_KEY_FAN]).entity_id == entity_id
+            ]
+            for d in devices:
+                device_service_func = getattr(d, service_func.__name__)
+                return await device_service_func(**service_data)
+
+        hass.services.async_register(
+            domain=domain,
+            service=service,
+            service_func=service_func_wrapper,
+            schema=schema,
+        )
+
+    device._register_services(wrapped_async_register)
 
 
-async def async_setup_platform(
+async def async_setup_platform_old(
     hass: HomeAssistant,
     config: ConfigType,
     async_add_entities: Callable[[List[Entity], bool], None],
