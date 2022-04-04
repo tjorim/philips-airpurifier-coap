e08a32f57254f43313935f7a845bbdee3b6261c6
kongo09 <kongo09@users.noreply.github.com>
2022-04-04 16:17:53 +0200

    make type explicit

--- custom_components/philips_airpurifier_coap/fan.py
+++ custom_components/philips_airpurifier_coap/fan.py
@@ -27,7 +27,7 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(
     hass: HomeAssistantType, 
     entry: ConfigEntry, 
-    async_add_entities: Callable
+    async_add_entities: Callable[[List[Entity], bool], None]
 ):
     _LOGGER.debug("async_setup_entry called for platform fan")
 
