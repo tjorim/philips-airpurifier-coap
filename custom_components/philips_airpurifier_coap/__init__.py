bf77d930e77dcc215586845c2926a2164d3fea33
kongo09 <kongo09@users.noreply.github.com>
2022-04-04 12:22:43 +0200

    clean up

--- custom_components/philips_airpurifier_coap/__init__.py
+++ custom_components/philips_airpurifier_coap/__init__.py
@@ -1,30 +1,23 @@
 """Support for Philips AirPurifier with CoAP."""
 from __future__ import annotations
 
-import asyncio
 import logging
 from struct import pack
 
 from aioairctrl import CoAPClient
 import voluptuous as vol
 
-from homeassistant.const import CONF_HOST, CONF_ICON, CONF_NAME
+from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
-from homeassistant.helpers import discovery
 import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.typing import ConfigType
 from homeassistant.config_entries import ConfigEntry
 
-from .philips import PhilipsEntity, Coordinator
+from .philips import Coordinator
 
 from .const import (
-    CONF_MODEL,
-    CONF_DEVICE_ID,
     DATA_KEY_CLIENT,
     DATA_KEY_COORDINATOR,
-    DEFAULT_ICON,
-    DEFAULT_NAME,
     DOMAIN,
 )
 
@@ -81,46 +74,3 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data[DOMAIN].pop(entry.data[CONF_HOST])
 
     return True
-
-
-# async def async_setup_old(hass: HomeAssistant, config: ConfigType) -> bool:
-#     """Set up the Philips AirPurifier integration."""
-#     hass.data[DOMAIN] = {}
-
-#     async def async_setup_air_purifier(conf: ConfigType):
-#         host = conf[CONF_HOST]
-
-#         _LOGGER.debug("Setting up %s integration with %s", DOMAIN, host)
-
-#         try:
-#             client = await CoAPClient.create(host)
-#         except Exception as ex:
-#             _LOGGER.warning(r"Failed to connect: %s", ex)
-#             raise ConfigEntryNotReady from ex
-
-#         coordinator = Coordinator(client)
-
-#         hass.data[DOMAIN][host] = {
-#             DATA_KEY_CLIENT: client,
-#             DATA_KEY_COORDINATOR: coordinator,
-#         }
-
-#         await coordinator.async_first_refresh()
-
-#         # autodetect model and name
-#         model = coordinator.status['type']
-#         name = coordinator.status['name']
-#         conf[CONF_MODEL] = model
-#         conf[CONF_NAME] = name
-#         _LOGGER.debug("Detected host %s as model %s with name: %s", host, model, name)
-
-#         for platform in PLATFORMS:
-#             hass.async_create_task(
-#                 discovery.async_load_platform(hass, platform, DOMAIN, conf, config)
-#             )
-
-#     tasks = [async_setup_air_purifier(conf) for conf in config[DOMAIN]]
-#     if tasks:
-#         await asyncio.wait(tasks)
-
-#     return True
