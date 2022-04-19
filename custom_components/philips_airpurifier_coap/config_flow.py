f8d28d43405d336a78e4c372eb0e69466ed538b1
kongo09 <kongo09@users.noreply.github.com>
2022-04-19 16:59:49 +0200

    dhcp step

--- custom_components/philips_airpurifier_coap/config_flow.py
+++ custom_components/philips_airpurifier_coap/config_flow.py
@@ -49,6 +49,106 @@ class PhilipsAirPurifierConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return schema
 
 
+    async def async_step_dhcp(self, discovery_info: dhcp.DhcpServiceInfo) -> FlowResult:
+        """Handle initial step of auto discovery flow."""
+        _LOGGER.debug(f"async_step_dhcp: called, found: {discovery_info}")
+
+        self._host = discovery_info.ip
+        _LOGGER.debug(f"trying to configure host: {self._host}")
+
+        # let's try and connect to an AirPurifier
+        try:
+            client = None
+
+            # try for 10s to get a valid client
+            async with async_timeout.timeout(10):
+                client = await CoAPClient.create(self._host)
+                _LOGGER.debug(f"got a valid client for host {self._host}")
+
+            # we give it 10s to get a status, otherwise we abort
+            # wrap the query for status
+            def get_status(client: CoAPClient):
+                _LOGGER.debug(f"trying to get status")
+                status = client.get_status()
+                _LOGGER.debug("got status")
+                return status
+
+            # now make this a process for 10s
+            async with async_timeout.timeout(10):
+                status = await get_status(client)
+
+            if client is not None:
+                await client.shutdown()
+
+            # get the status out of the queue
+            _LOGGER.debug(f"status for host {self._host} is: {status}")
+
+        except asyncio.TimeoutError:
+            _LOGGER.warning(r"Host %s doesn't answer like a supported Philips AirPurifier, aborting", self._host)
+            self.async_abort(reason="model_unsupported")
+
+        except Exception as ex:
+            _LOGGER.warning(r"Failed to connect: %s", ex)
+            raise exceptions.ConfigEntryNotReady from ex
+
+        # autodetect model and name
+        self._model = status['type']
+        self._name = status['name']
+        self._device_id = status['DeviceId']
+        _LOGGER.debug("Detected host %s as model %s with name: %s", self._host, self._model, self._name)
+
+        # check if model is supported
+        if not self._model in model_to_class.keys():
+            _LOGGER.warn(f"Model {self._model} found, but not supported. Aborting discovery.")
+            return self.async_abort(reason="model_unsupported")
+
+        # use the device ID as unique_id
+        unique_id = self._device_id
+        _LOGGER.debug(f"async_step_user: unique_id={unique_id}")
+
+        # set the unique id for the entry, abort if it already exists
+        await self.async_set_unique_id(unique_id)
+        self._abort_if_unique_id_configured()
+
+        # store the data for the next step to get confirmation
+        self.context.update({
+            "title_placeholders": {
+                CONF_NAME: self._model + " " + self._name,
+            }
+        })
+
+        # show the confirmation form to the user
+        _LOGGER.debug(f"waiting for async_step_dhcp_confirm")
+        return await self.async_step_dhcp_confirm()
+
+
+    async def async_step_dhcp_confirm(
+        self,
+        user_input: dict[str, Any] = None
+    ) -> FlowResult:
+        """Confirm the dhcp discovered data."""
+        _LOGGER.debug(f"async_step_dhcp_confirm called with user_input: {user_input}")
+
+        # user input was provided, so check and save it
+        if user_input is not None:
+            _LOGGER.debug(f"entered creation for model {self._model} with name '{self._name}' at {self._host}")
+            user_input[CONF_MODEL] = self._model
+            user_input[CONF_NAME] = self._name
+            user_input[CONF_DEVICE_ID] = self._device_id
+            user_input[CONF_HOST] = self._host
+
+            return self.async_create_entry(
+                title=self._model + " " + self._name,
+                data=user_input
+            )
+
+        _LOGGER.debug(f"showing form")
+        # show the form to the user
+        return self.async_show_form(
+            step_id="dhcp_confirm",
+        )
+
+
     async def async_step_user(self, user_input: dict[str, Any] = None) -> FlowResult:
         """Handle initial step of user config flow."""
 
