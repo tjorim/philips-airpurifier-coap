c153f871af3bd490ea955d8a84918b34d1ec5d54
kongo09 <kongo09@users.noreply.github.com>
2022-05-03 14:11:48 +0200

    remove registering services

--- custom_components/philips_airpurifier_coap/fan.py
+++ custom_components/philips_airpurifier_coap/fan.py
@@ -2,19 +2,16 @@
 from __future__ import annotations
 
 import logging
-from typing import Callable, Optional, List
-
-import voluptuous as vol
 
 from homeassistant.const import CONF_HOST, CONF_NAME
 from homeassistant.helpers.typing import HomeAssistantType
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.config_entries import ConfigEntry
 
 
 from .philips import model_to_class
 from .const import (
     CONF_MODEL,
-    DATA_KEY_CLIENT,
     DATA_KEY_COORDINATOR,
     DATA_KEY_FAN,
     DOMAIN,
@@ -26,7 +23,7 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(
     hass: HomeAssistantType, 
     entry: ConfigEntry, 
-    async_add_entities: Callable[[List[Entity], bool], None]
+    async_add_entities: AddEntitiesCallback,
 ):
     _LOGGER.debug("async_setup_entry called for platform fan")
 
@@ -35,11 +32,12 @@ async def async_setup_entry(
     name = entry.data[CONF_NAME]
 
     data = hass.data[DOMAIN][host]
+    coordinator = data[DATA_KEY_COORDINATOR]
 
     model_class = model_to_class.get(model)
     if model_class:
         device = model_class(
-            data[DATA_KEY_COORDINATOR],
+            coordinator,
             model=model,
             name=name,
         )
@@ -49,30 +47,3 @@ async def async_setup_entry(
 
     data[DATA_KEY_FAN] = device
     async_add_entities([device], update_before_add=True)
-
-    def wrapped_async_register(
-        domain: str,
-        service: str,
-        service_func: Callable,
-        schema: Optional[vol.Schema] = None,
-    ):
-        async def service_func_wrapper(service_call):
-            service_data = service_call.data.copy()
-            entity_id = service_data.pop("entity_id", None)
-            devices = [
-                d
-                for entry in hass.data[DOMAIN].values()
-                if (d := entry[DATA_KEY_FAN]).entity_id == entity_id
-            ]
-            for d in devices:
-                device_service_func = getattr(d, service_func.__name__)
-                return await device_service_func(**service_data)
-
-        hass.services.async_register(
-            domain=domain,
-            service=service,
-            service_func=service_func_wrapper,
-            schema=schema,
-        )
-
-    device._register_services(wrapped_async_register)
