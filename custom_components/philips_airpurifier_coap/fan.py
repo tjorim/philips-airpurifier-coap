12692d56c1994e7f0b556a2efb7e73b047d3c661
kongo09 <kongo09@users.noreply.github.com>
2022-05-11 12:11:58 +0200

    eliminate service registration

--- custom_components/philips_airpurifier_coap/fan.py
+++ custom_components/philips_airpurifier_coap/fan.py
@@ -49,30 +49,3 @@ async def async_setup_entry(
 
     data[DATA_KEY_FAN] = device
     async_add_entities([device], update_before_add=True)
-
-    def wrapped_async_register(
-        domain: str,
-        service: str,
-        service_func: Callable,
-        schema: Optional[vol.Schema] = None,
-    ):
-        async def service_func_wrapper(service_call):
-            service_data = service_call.data.copy()
-            entity_id = service_data.pop("entity_id", None)
-            devices = [
-                d
-                for entry in hass.data[DOMAIN].values()
-                if (d := entry[DATA_KEY_FAN]).entity_id == entity_id
-            ]
-            for d in devices:
-                device_service_func = getattr(d, service_func.__name__)
-                return await device_service_func(**service_data)
-
-        hass.services.async_register(
-            domain=domain,
-            service=service,
-            service_func=service_func_wrapper,
-            schema=schema,
-        )
-
-    device._register_services(wrapped_async_register)
