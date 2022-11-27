4945d402fe38fa57b490ef604b7697f0f068eeee
kongo09 <kongo09@users.noreply.github.com>
2022-11-27 20:39:53 +0100

    fix pollution and allergen mode for AC4558

--- custom_components/philips_airpurifier_coap/philips.py
+++ custom_components/philips_airpurifier_coap/philips.py
@@ -631,8 +631,9 @@ class PhilipsAC4558(PhilipsGenericCoAPFan):
         # there doesn't seem to be a manual mode, so no speed setting as part of preset
         PRESET_MODE_AUTO: {PHILIPS_POWER: "1", PHILIPS_MODE: "AG"},
         PRESET_MODE_GAS: {PHILIPS_POWER: "1", PHILIPS_MODE: "F"},
-        PRESET_MODE_POLLUTION: {PHILIPS_POWER: "1", PHILIPS_MODE: "p"},
-        PRESET_MODE_ALLERGEN: {PHILIPS_POWER: "1", PHILIPS_MODE: "A"},
+        # it seems that when setting the pollution and allergen modes, we also need to set speed "a"
+        PRESET_MODE_POLLUTION: {PHILIPS_POWER: "1", PHILIPS_MODE: "p", PHILIPS_SPEED: "a"},
+        PRESET_MODE_ALLERGEN: {PHILIPS_POWER: "1", PHILIPS_MODE: "A", PHILIPS_SPEED: "a"},
     }
     AVAILABLE_SPEEDS = {
         PRESET_MODE_SLEEP: {PHILIPS_POWER: "1", PHILIPS_SPEED: "s"},
