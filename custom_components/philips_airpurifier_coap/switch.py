aac85e7fd8b2c528f95202e2d75c3b81764afde4
kongo09 <kongo09@users.noreply.github.com>
2024-01-05 13:30:38 +0100

    fix warnings

--- custom_components/philips_airpurifier_coap/switch.py
+++ custom_components/philips_airpurifier_coap/switch.py
@@ -1,36 +1,34 @@
-"""Philips Air Purifier & Humidifier Switches"""
+"""Philips Air Purifier & Humidifier Switches."""
 from __future__ import annotations
 
+from collections.abc import Callable
 import logging
-from typing import Any, Callable, List
-
-from aioairctrl import CoAPClient
+from typing import Any
 
 from homeassistant.components.switch import SwitchEntity
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     ATTR_DEVICE_CLASS,
     ATTR_ICON,
+    CONF_ENTITY_CATEGORY,
     CONF_HOST,
     CONF_NAME,
-    CONF_ENTITY_CATEGORY,
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import PlatformNotReady
 from homeassistant.helpers.entity import Entity
-from homeassistant.config_entries import ConfigEntry
 
-from .philips import Coordinator, PhilipsEntity, model_to_class
 from .const import (
     ATTR_LABEL,
-    SWITCH_ON,
-    SWITCH_OFF,
     CONF_MODEL,
-    DATA_KEY_CLIENT,
     DATA_KEY_COORDINATOR,
     DOMAIN,
     PHILIPS_DEVICE_ID,
+    SWITCH_OFF,
+    SWITCH_ON,
     SWITCH_TYPES,
 )
+from .philips import Coordinator, PhilipsEntity, model_to_class
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -38,8 +36,9 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(
     hass: HomeAssistant,
     entry: ConfigEntry,
-    async_add_entities: Callable[[List[Entity], bool], None]
+    async_add_entities: Callable[[list[Entity], bool], None],
 ) -> None:
+    """Set up platform for switch."""
     _LOGGER.debug("async_setup_entry called for platform switch")
 
     host = entry.data[CONF_HOST]
@@ -52,11 +51,10 @@ async def async_setup_entry(
 
     model_class = model_to_class.get(model)
     if model_class:
-
         available_switches = []
-        
+
         for cls in reversed(model_class.__mro__):
-            cls_available_switches = getattr(cls, "AVAILABLE_SWITCHES", [])          
+            cls_available_switches = getattr(cls, "AVAILABLE_SWITCHES", [])
             available_switches.extend(cls_available_switches)
 
         switches = []
@@ -77,12 +75,8 @@ class PhilipsSwitch(PhilipsEntity, SwitchEntity):
 
     _attr_is_on: bool | None = False
 
-    def __init__(
-        self,
-        coordinator: Coordinator,
-        name: str,
-        model: str,
-        switch: str
+    def __init__(  # noqa: D107
+        self, coordinator: Coordinator, name: str, model: str, switch: str
     ) -> None:
         super().__init__(coordinator)
         self._model = model
@@ -91,7 +85,9 @@ class PhilipsSwitch(PhilipsEntity, SwitchEntity):
         self._off = self._description.get(SWITCH_OFF)
         self._attr_device_class = self._description.get(ATTR_DEVICE_CLASS)
         self._attr_icon = self._description.get(ATTR_ICON)
-        self._attr_name = f"{name} {self._description[ATTR_LABEL].replace('_', ' ').title()}"
+        self._attr_name = (
+            f"{name} {self._description[ATTR_LABEL].replace('_', ' ').title()}"
+        )
         self._attr_entity_category = self._description.get(CONF_ENTITY_CATEGORY)
 
         try:
@@ -103,17 +99,17 @@ class PhilipsSwitch(PhilipsEntity, SwitchEntity):
         self._attrs: dict[str, Any] = {}
         self.kind = switch
 
-
     @property
     def is_on(self) -> bool:
+        """Return if switch is on."""
         return self._device_status.get(self.kind) == self._on
 
-
     async def async_turn_on(self, **kwargs) -> None:
+        """Switch the switch on."""
         _LOGGER.debug("async_turn_on, kind: %s - value: %s", self.kind, self._on)
         await self.coordinator.client.set_control_value(self.kind, self._on)
 
-
     async def async_turn_off(self, **kwargs) -> None:
+        """Switch the switch off."""
         _LOGGER.debug("async_turn_off, kind: %s - value: %s", self.kind, self._off)
         await self.coordinator.client.set_control_value(self.kind, self._off)
