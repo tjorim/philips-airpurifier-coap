0952b31889efa2619f4ed3b66a129515e1b9b1ab
kongo09 <kongo09@users.noreply.github.com>
2024-01-05 09:58:13 +0100

    fix some warnings

--- custom_components/philips_airpurifier_coap/light.py
+++ custom_components/philips_airpurifier_coap/light.py
@@ -1,41 +1,35 @@
-"""Philips Air Purifier & Humidifier Switches"""
+"""Philips Air Purifier & Humidifier Switches."""
 from __future__ import annotations
 
+from collections.abc import Callable
 import logging
-from typing import Any, Callable, List
+from typing import Any
 
-from aioairctrl import CoAPClient
-
-from homeassistant.components.light import (
-    LightEntity,
-    ATTR_BRIGHTNESS,
-    COLOR_MODE_BRIGHTNESS,
-    COLOR_MODE_ONOFF,
-)
+from homeassistant.components.light import ATTR_BRIGHTNESS, ColorMode, LightEntity
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     ATTR_DEVICE_CLASS,
     ATTR_ICON,
+    CONF_ENTITY_CATEGORY,
     CONF_HOST,
     CONF_NAME,
-    CONF_ENTITY_CATEGORY,
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import PlatformNotReady
 from homeassistant.helpers.entity import Entity
-from homeassistant.config_entries import ConfigEntry
 
-from .philips import Coordinator, PhilipsEntity, model_to_class
 from .const import (
     ATTR_LABEL,
-    DIMMABLE,
-    SWITCH_ON,
-    SWITCH_OFF,
     CONF_MODEL,
     DATA_KEY_COORDINATOR,
+    DIMMABLE,
     DOMAIN,
-    PHILIPS_DEVICE_ID,
     LIGHT_TYPES,
+    PHILIPS_DEVICE_ID,
+    SWITCH_OFF,
+    SWITCH_ON,
 )
+from .philips import Coordinator, PhilipsEntity, model_to_class
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -43,9 +37,10 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(
     hass: HomeAssistant,
     entry: ConfigEntry,
-    async_add_entities: Callable[[List[Entity], bool], None]
+    async_add_entities: Callable[[list[Entity], bool], None],
 ) -> None:
-    _LOGGER.debug("async_setup_entry called for platform switch")
+    """Set up the light platform."""
+    _LOGGER.debug("async_setup_entry called for platform light")
 
     host = entry.data[CONF_HOST]
     model = entry.data[CONF_MODEL]
@@ -57,13 +52,12 @@ async def async_setup_entry(
 
     model_class = model_to_class.get(model)
     if model_class:
-
         available_lights = []
-        
+
         for cls in reversed(model_class.__mro__):
-            cls_available_lights = getattr(cls, "AVAILABLE_LIGHTS", [])          
+            cls_available_lights = getattr(cls, "AVAILABLE_LIGHTS", [])
             available_lights.extend(cls_available_lights)
-        
+
         lights = []
 
         for light in LIGHT_TYPES:
@@ -82,12 +76,8 @@ class PhilipsLight(PhilipsEntity, LightEntity):
 
     _attr_is_on: bool | None = False
 
-    def __init__(
-        self,
-        coordinator: Coordinator,
-        name: str,
-        model: str,
-        light: str
+    def __init__(  # noqa: D107
+        self, coordinator: Coordinator, name: str, model: str, light: str
     ) -> None:
         super().__init__(coordinator)
         self._model = model
@@ -97,18 +87,20 @@ class PhilipsLight(PhilipsEntity, LightEntity):
         self._dimmable = self._description.get(DIMMABLE)
         self._attr_device_class = self._description.get(ATTR_DEVICE_CLASS)
         self._attr_icon = self._description.get(ATTR_ICON)
-        self._attr_name = f"{name} {self._description[ATTR_LABEL].replace('_', ' ').title()}"
+        self._attr_name = (
+            f"{name} {self._description[ATTR_LABEL].replace('_', ' ').title()}"
+        )
         self._attr_entity_category = self._description.get(CONF_ENTITY_CATEGORY)
 
-        if self._dimmable == None:
+        if self._dimmable is None:
             self._dimmable = False
 
         if self._dimmable:
-            self._attr_color_mode = COLOR_MODE_BRIGHTNESS
-            self._attr_supported_color_modes = {COLOR_MODE_BRIGHTNESS}
+            self._attr_color_mode = ColorMode.BRIGHTNESS
+            self._attr_supported_color_modes = {ColorMode.BRIGHTNESS}
         else:
-            self._attr_color_mode = COLOR_MODE_ONOFF
-            self._attr_supported_color_modes = {COLOR_MODE_ONOFF}
+            self._attr_color_mode = ColorMode.ONOFF
+            self._attr_supported_color_modes = {ColorMode.ONOFF}
 
         try:
             device_id = self._device_status[PHILIPS_DEVICE_ID]
@@ -119,9 +111,9 @@ class PhilipsLight(PhilipsEntity, LightEntity):
         self._attrs: dict[str, Any] = {}
         self.kind = light
 
-
     @property
     def is_on(self) -> bool:
+        """Return if the light is on."""
         status = self._device_status.get(self.kind)
         # _LOGGER.debug("is_on, kind: %s - status: %s - on: %s", self.kind, status, self._on)
         if self._dimmable:
@@ -129,17 +121,17 @@ class PhilipsLight(PhilipsEntity, LightEntity):
         else:
             return status == self._on
 
-
     @property
     def brightness(self) -> int | None:
+        """Return the brightness of the light."""
         if self._dimmable:
             brightness = self._device_status.get(self.kind)
-            return round(255 *brightness / 100)
+            return round(255 * brightness / 100)
         else:
             return None
 
-
     async def async_turn_on(self, **kwargs) -> None:
+        """Turn the light on."""
         if self._dimmable:
             if ATTR_BRIGHTNESS in kwargs:
                 value = int(100 * kwargs[ATTR_BRIGHTNESS] / 255)
@@ -151,7 +143,7 @@ class PhilipsLight(PhilipsEntity, LightEntity):
         _LOGGER.debug("async_turn_on, kind: %s - value: %s", self.kind, value)
         await self.coordinator.client.set_control_value(self.kind, value)
 
-
     async def async_turn_off(self, **kwargs) -> None:
+        """Turn the light off."""
         _LOGGER.debug("async_turn_off, kind: %s - value: %s", self.kind, self._off)
         await self.coordinator.client.set_control_value(self.kind, self._off)
